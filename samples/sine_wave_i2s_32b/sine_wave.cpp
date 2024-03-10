80cbbdfe561dafc85ebb83986c4028a31f64589b
elehobica <elehobica@gmail.com>
2024-03-10 23:34:10 +0900

    change into library

--- samples/sine_wave_i2s_32b/sine_wave.cpp
+++ samples/sine_wave_i2s_32b/sine_wave.cpp
@@ -7,33 +7,46 @@
 #include <stdio.h>
 #include <math.h>
 
-#if PICO_ON_DEVICE
-
 #include "hardware/pll.h"
 #include "hardware/clocks.h"
 #include "hardware/structs/clocks.h"
-
-#endif
+#include "hardware/pio.h"
 
 #include "pico/stdlib.h"
-
-#if USE_AUDIO_I2S
-
+#include "pico/audio.h"
 #include "pico/audio_i2s.h"
-
-#elif USE_AUDIO_PWM
-#include "pico/audio_pwm.h"
-#elif USE_AUDIO_SPDIF
-#include "pico/audio_spdif.h"
-#endif
 
 #define SINE_WAVE_TABLE_LEN 2048
 #define SAMPLES_PER_BUFFER 1156 // Samples / channel
 
 static const uint32_t PIN_DCDC_PSM_CTRL = 23;
 
-static int16_t sine_wave_table[SINE_WAVE_TABLE_LEN];
 audio_buffer_pool_t *ap;
+static bool decode_flg = false;
+static constexpr int32_t DAC_ZERO = 1;
+
+#define audio_pio __CONCAT(pio, PICO_AUDIO_I2S_PIO)
+
+static audio_format_t audio_format = {
+    .sample_freq = 44100,
+    .pcm_format = AUDIO_PCM_FORMAT_S32,
+    .channel_count = AUDIO_CHANNEL_STEREO
+};
+
+static audio_buffer_format_t producer_format = {
+    .format = &audio_format,
+    .sample_stride = 8
+};
+
+static audio_i2s_config_t i2s_config = {
+    .data_pin = PICO_AUDIO_I2S_DATA_PIN,
+    .clock_pin_base = PICO_AUDIO_I2S_CLOCK_PIN_BASE,
+    .dma_channel0 = 0,
+    .dma_channel1 = 1,
+    .pio_sm = 0
+};
+
+static int16_t sine_wave_table[SINE_WAVE_TABLE_LEN];
 uint32_t step0 = 0x200000;
 uint32_t step1 = 0x200000;
 uint32_t pos0 = 0;
@@ -41,6 +54,7 @@ uint32_t pos1 = 0;
 const uint32_t pos_max = 0x10000 * SINE_WAVE_TABLE_LEN;
 uint vol = 20;
 
+#if 0
 audio_buffer_pool_t *init_audio() {
 
     static audio_format_t audio_format = {
@@ -104,18 +118,77 @@ audio_buffer_pool_t *init_audio() {
 #endif
     return producer_pool;
 }
+#endif
 
 static inline uint32_t _millis(void)
 {
 	return to_ms_since_boot(get_absolute_time());
 }
 
+void i2s_audio_deinit()
+{
+    decode_flg = false;
+
+    audio_i2s_set_enabled(false);
+    audio_i2s_end();
+
+    audio_buffer_t* ab;
+    ab = take_audio_buffer(ap, false);
+    while (ab != nullptr) {
+        free(ab->buffer->bytes);
+        free(ab->buffer);
+        ab = take_audio_buffer(ap, false);
+    }
+    ab = get_free_audio_buffer(ap, false);
+    while (ab != nullptr) {
+        free(ab->buffer->bytes);
+        free(ab->buffer);
+        ab = get_free_audio_buffer(ap, false);
+    }
+    ab = get_full_audio_buffer(ap, false);
+    while (ab != nullptr) {
+        free(ab->buffer->bytes);
+        free(ab->buffer);
+        ab = get_full_audio_buffer(ap, false);
+    }
+    free(ap);
+    ap = nullptr;
+}
+
+audio_buffer_pool_t *i2s_audio_init(uint32_t sample_freq)
+{
+    audio_format.sample_freq = sample_freq;
+
+    audio_buffer_pool_t *producer_pool = audio_new_producer_pool(&producer_format, 3, SAMPLES_PER_BUFFER);
+    ap = producer_pool;
+
+    bool __unused ok;
+    const audio_format_t *output_format;
+
+    output_format = audio_i2s_setup(&audio_format, &audio_format, &i2s_config);
+    if (!output_format) {
+        panic("PicoAudio: Unable to open audio device.\n");
+    }
+
+    ok = audio_i2s_connect(producer_pool);
+    assert(ok);
+    { // initial buffer data
+        audio_buffer_t *ab = take_audio_buffer(producer_pool, true);
+        int32_t *samples = (int32_t *) ab->buffer->bytes;
+        for (uint i = 0; i < ab->max_sample_count; i++) {
+            samples[i*2+0] = DAC_ZERO;
+            samples[i*2+1] = DAC_ZERO;
+        }
+        ab->sample_count = ab->max_sample_count;
+        give_audio_buffer(producer_pool, ab);
+    }
+    audio_i2s_set_enabled(true);
+
+    decode_flg = true;
+    return producer_pool;
+}
+
 int main() {
-#if PICO_ON_DEVICE
-#if USE_AUDIO_PWM
-    set_sys_clock_48mhz();
-#endif
-#endif
 
     stdio_init_all();
 
@@ -152,12 +225,9 @@ int main() {
         sine_wave_table[i] = 32767 * cosf(i * 2 * (float) (M_PI / SINE_WAVE_TABLE_LEN));
     }
 
-    ap = init_audio();
+    ap = i2s_audio_init(44100);
 
     while (true) {
-#if USE_AUDIO_PWM
-        enum audio_correction_mode m = audio_pwm_get_correction_mode();
-#endif
         int c = getchar_timeout_us(0);
         if (c >= 0) {
             if (c == '-' && vol) vol--;
@@ -167,28 +237,14 @@ int main() {
             if (c == '{' && step1 > 0x10000) step1 -= 0x10000;
             if (c == '}' && step1 < (SINE_WAVE_TABLE_LEN / 16) * 0x20000) step1 += 0x10000;
             if (c == 'q') break;
-#if USE_AUDIO_PWM
-            if (c == 'c') {
-                bool done = false;
-                while (!done) {
-                    if (m == none) m = fixed_dither;
-                    else if (m == fixed_dither) m = dither;
-                    else if (m == dither) m = noise_shaped_dither;
-                    else if (m == noise_shaped_dither) m = none;
-                    done = audio_pwm_set_correction_mode(m);
-                }
-            }
-            printf("vol = %d, step0 = %d, step1 = %d mode = %d      \r", vol, step0 >> 16, step1 >> 16, m);
-#else
             printf("vol = %d, step0 = %d, step1 = %d      \r", vol, step0 >> 16, step1 >> 16);
-#endif
         }
     }
     puts("\n");
     return 0;
 }
 
-void i2s_callback_func()
+void decode()
 {
     audio_buffer_t *buffer = take_audio_buffer(ap, false);
     if (buffer == NULL) { return; }
@@ -207,4 +263,17 @@ void i2s_callback_func()
     buffer->sample_count = buffer->max_sample_count;
     give_audio_buffer(ap, buffer);
     return;
+}
+
+extern "C" {
+// callback from:
+//   void __isr __time_critical_func(audio_i2s_dma_irq_handler)()
+//   defined at my_pico_audio_i2s/audio_i2s.c
+//   where i2s_callback_func() is declared with __attribute__((weak))
+void i2s_callback_func()
+{
+    if (decode_flg) {
+        decode();
+    }
+}
 }
